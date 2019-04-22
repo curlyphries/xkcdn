70af41b85573d97819a3eea25ecd21c8867dd64d
BuildTools <noah@danielgarcia.info>
2019-04-22 16:48:06 -0500

    Comments. resolves #1

--- xkcdn.py
+++ xkcdn.py
@@ -2,14 +2,14 @@ import click
 import json
 import requests
 
-@click.command()
-@click.option('--output', default=None, help='Output file')
+@click.command() # See click decorators
+@click.option('--output', default=None, help='Output file') # output file
 def cli(output):
-    r = requests.get("http://xkcd.com/info.0.json")
-    data = json.loads(r.text)
-    if (output == None):
-        file = open(str(data['num']) + ".png", 'wb')
+    r = requests.get("http://xkcd.com/info.0.json") # get latest comic
+    data = json.loads(r.text) # make it a dict
+    if (output == None): ## If no output is given, give it the comic number
+        file = open(str(data['num']) + ".png", 'wb') # kludgy way to take the number and append .png
     else:
         file = open(str(output), 'wb')
-    res = requests.get(data["img"])
-    file.write(res.content)
+    res = requests.get(data["img"]) # get the comic
+    file.write(res.content) # write it.
