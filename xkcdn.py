e91c13f05df08858867323ff12f24c4eef16757f
Shrubhog <noah@danielgarcia.info>
2019-04-23 10:39:44 -0500

    add custom number option Resolves #2

--- xkcdn.py
+++ xkcdn.py
@@ -4,8 +4,15 @@ import requests
 
 @click.command() # See click decorators
 @click.option('--output', default=None, help='Output file') # output file
-def cli(output):
-    r = requests.get("http://xkcd.com/info.0.json") # get latest comic
+@click.option('--num', default=None, help='Comic number to retrieve')
+def cli(output, num):
+    if (num == None):
+        r = requests.get("http://xkcd.com/info.0.json") # get latest comic
+    else:
+        try: 
+            r = requests.get("https://xkcd.com/{}/info.0.json".format(str(num)))
+        except requests.ConnectionError:
+            print("Comic Not Found")
     data = json.loads(r.text) # make it a dict
     if (output == None): ## If no output is given, give it the comic number
         file = open(str(data['num']) + ".png", 'wb') # kludgy way to take the number and append .png
