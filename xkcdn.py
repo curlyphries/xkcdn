182a318931f5a7657e1d700a8b6ca40a3ff516e7
Shrubhog <noah@danielgarcia.info>
2019-04-26 13:58:58 -0500

    random Resolves #11

--- xkcdn.py
+++ xkcdn.py
@@ -3,16 +3,24 @@ import json
 import requests
 from PIL import Image
 from io import BytesIO
+import random
 
 @click.command() # See click decorators
 @click.option('--output', default=None, help='Output file') # output file
 @click.option('--num', default=None, help='Comic number to retrieve')
 @click.option('--popup', flag_value=True, default=False, help="Show in Pillow window")
-def cli(output, num, popup):
+@click.option('--random', 'randYN', flag_value=True, default=False, help="Get random comic")
+def cli(output, num, popup, randYN):
     if (output != None and popup == True):
         raise Exception("Cannot download and not download comic.")
-    if (num == None):
+    if (num == None and randYN != True):
         r = requests.get("http://xkcd.com/info.0.json") # get latest comic
+    elif (randYN == True):
+        ra = requests.get("http://xkcd.com/info.0.json")
+        raData = json.loads(ra.text)
+        raRes = int(raData["num"])
+        num = random.randint(1, raRes+1)
+        r = requests.get("https://xkcd.com/{}/info.0.json".format(num))
     else:
         try: 
             r = requests.get("https://xkcd.com/{}/info.0.json".format(num))
